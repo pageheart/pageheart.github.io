6c7903a4fb1744e49080e408bdca1b3c18f52097
pageheart <61791188+pageheart@users.noreply.github.com>
2021-08-28 00:42:46 +0900

    Update component.js

--- vue/component.js
+++ vue/component.js
@@ -1,5 +1,15 @@
 Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
 })
 
-var app = new Vue(...)
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})

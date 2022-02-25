1eb850dc83d719d4c850c43dbf4d965d47757055
pageheart <61791188+pageheart@users.noreply.github.com>
2022-02-25 18:00:57 +0900

    Update index.js

--- vue/index.js
+++ vue/index.js
@@ -48,3 +48,47 @@ var app6 = new Vue({
     message: '텍스트 연동'
   }
 })
+
+var insuranceContractNo = new Vue({
+    el: '#insuranceContractNo',
+    data: {
+        insNo: '',
+        inputDate: (new Date()).getFullYear() + '' + (((new Date()).getMonth()+1) < 10 ? '0'+((new Date()).getMonth()+1) : ((new Date()).getMonth()+1)) + ((new Date()).getMonth() < 10 ? '0'+(new Date()).getMonth() : (new Date()).getMonth())
+    },
+    methods: {
+        sendScan: function () {
+            if(this.insNo == '') {
+                alert("계약번호를 입력 해주세요.");
+                this.$refs.insNo.focus();
+            }
+            else if(this.inputDate == '') {
+                alert("날짜를 입력 해주세요.");
+                this.$refs.inputDate.focus();
+            }
+            else {
+                const requestOptions = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ 
+                        insNo: this.insNo,
+                        inputDate : this.inputDate
+                     })
+                };
+                //fetch('https://jsonplaceholder.typicode.com/posts/1', requestOptions) //POST로 보내면 404에러남...
+                fetch('https://jsonplaceholder.typicode.com/posts/1')
+                    .then((response) => {
+                        if(response.ok){
+                            return response.json();
+                        }
+                        throw new Error('Network response was not ok');
+                    })
+                    .then((json) => {
+                        console.log(json);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            }
+        }
+    }
+})
